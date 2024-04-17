336d27df959bc1ae3206f1182b97b976a72d1d0f
gangadharDell <ganagdharDell@gmail.com>
2024-04-17 16:45:26 +0530

    updated the code

--- vpc.tf
+++ vpc.tf
@@ -17,31 +17,23 @@ resource "aws_vpc" "main" {
 # Create your subnets here
 # =========================
 
-module "subnets_cidr" {
-  source              = "hashicorp/subnets/cidr"
-  version             = "1.2.0"
-  availability_zone   = var.availability_zone
-  vpc_cidr_block      = aws_vpc.main.cidr_block
-  number_of_subnets   = 2
-  netmask_length      = 24
+ # Create public subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "${cidrsubnet(var.vpc_cidr, 4, 0)}"
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true  # Enable auto-assign public IP addresses
+  tags = {
+    Name ="Public Subnet"
   }
-
-  # Public Subnet
-  resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = module.subnets_cidr.cidr_blocks[0]
-  availability_zone = var.availability_zone
-  map_public_ip_on_launch = true
-
-  tags = module.label_subnet_public.tags
 }
 
-# This subnet is not accessible by the public internet
-
+# Create private subnet
 resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = module.subnets_cidr.cidr_blocks[1]
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "${cidrsubnet(var.vpc_cidr, 4, 1)}"
   availability_zone = var.availability_zone
-  map_public_ip_on_launch = false
-  tags = module.label_subnet_private.tags
+  tags = {
+    Name = "Private Subnet"
+  }
 }
