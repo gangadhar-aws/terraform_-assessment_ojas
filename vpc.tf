d0410c7c35e731836f495dcb2af93f9cbe53e31a
gangadharDell <ganagdharDell@gmail.com>
2024-04-18 09:36:54 +0530

    create route table and internet gateway for subnets

--- vpc.tf
+++ vpc.tf
@@ -17,15 +17,20 @@ resource "aws_vpc" "main" {
 # Create your subnets here
 # =========================
 
+# Create internet gateway
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.main.id
+  tags = module.label_vpc.tags
+}
+
+
  # Create public subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "${cidrsubnet(var.vpc_cidr, 4, 0)}"
   availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true  # Enable auto-assign public IP addresses
-  tags = {
-    Name ="Public Subnet"
-  }
+  map_public_ip_on_launch = true   
+  tags = module.label_vpc.tags
 }
 
 # Create private subnet
@@ -33,7 +38,35 @@ resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "${cidrsubnet(var.vpc_cidr, 4, 1)}"
   availability_zone = var.availability_zone
-  tags = {
-    Name = "Private Subnet"
+  tags = module.label_vpc.tags
+}
+
+# Create route table for public subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = module.label_vpc.tags
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
   }
+}
+
+# Associate public subnet with public route table
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# Create route table for private subnet
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = module.label_vpc.tags
+}
+
+# Associate private subnet with private route table
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
